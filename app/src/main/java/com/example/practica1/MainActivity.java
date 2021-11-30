01b7dfbfe21d69d84cf9a8dcd7499f72ceb684d9
VivianZamora <vivian.zamora2017@uteq.edu.ec>
2021-11-30 12:30:43 -0500

    avances

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -11,4 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btEnviar(View view){
+        //Creamos el Intent
+        Intent intent = new Intent(actMain.this, actpantalla2.class);
+        EditText txtNombre = (EditText)findViewById(R.id.txPtNombre);
+        //Creamos la información a pasar entre actividades - Pares Key-Value
+        Bundle b = new Bundle();
+
+        b.putString("NOMBRE", txtPNombre.getText().toString());
+        //Añadimos la información al intent
+        intent.putExtras(b);
+        // Iniciamos la nueva actividad
+        startActivity(intent);
+    }
+
+
 }
